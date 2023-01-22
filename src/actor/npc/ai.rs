b7c9fc52557d50db78d0f120706daf13265564c5
Leslie <XXX>
2023-01-22 05:09:58 +0100

    split up AI

--- src/actor/npc/ai.rs
+++ src/actor/npc/ai.rs
@@ -8,56 +8,101 @@ pub struct AIPlugin;
 impl Plugin for AIPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system_to_stage(StartupStage::PostStartup, init)
-            .add_system(movement)
-            .add_system(debug_loop);
+            .add_system(work_follow_main_task_rotation)
+            .add_system(work_move_to_sub_task)
+            .add_system(follow_main_task_movement_sub_task_planning);
     }
 }
 
-pub struct PositionalTask {
-    target_translation: Vec3,
+pub enum Task {
+    Idle,
+    Follow(Entity),
+    MoveTo(Vec3),
+}
+impl Default for Task {
+    fn default() -> Self {
+        Task::Idle
+    }
 }
 
-#[derive(Component, Default)]
-pub struct PositionalTaskQueue {
-    pub queue: Vec<PositionalTask>,
+pub enum SubTask {
+    MoveTo(Vec3),
+    KeepDistance(Vec3, f32),
 }
 
-fn movement(mut npc_query: Query<(&Stats, &mut Transform, &PositionalTaskQueue)>, time: Res<Time>) {
-    for (stats, mut transform, task_queue) in npc_query.iter_mut() {
-        if let Some(task) = task_queue.queue.first() {
-            let (up_axis_rotation, _, _) = transform
-                .looking_at(task.target_translation, Vec3::Y)
+#[derive(Default, Component)]
+pub struct AIMovementTaskQueue {
+    pub main_task: Task,
+    pub sub_task_queue: Vec<SubTask>,
+}
+
+fn work_follow_main_task_rotation(
+    npc_query: Query<(Entity, &mut AIMovementTaskQueue)>,
+    mut transforms: Query<&mut Transform>,
+) {
+    const EULER: EulerRot = EulerRot::YXZ;
+    for (npc, task_queue) in npc_query.iter() {
+        if let Task::Follow(target) = task_queue.main_task {
+            let target_translation = transforms
+                .get(target)
+                .expect("Could not find target entity")
+                .translation;
+            let mut npc_transform = transforms.get_mut(npc).expect("Cannot find NPC");
+
+            let (y_rot, _, _) = npc_transform
+                .looking_at(target_translation, npc_transform.up())
                 .rotation
-                .to_euler(EulerRot::default());
+                .to_euler(EULER);
 
-            transform.rotation = Quat::from_euler(EulerRot::default(), up_axis_rotation, 0.0, 0.0);
-
-            let mut direction = Vec3::ZERO;
-            let velocity = stats.get_movement_velocity();
-            direction += transform.forward();
-            transform.translation += direction * velocity * time.delta_seconds();
+            npc_transform.rotation = Quat::from_euler(EULER, y_rot, 0.0, 0.0);
         }
     }
 }
 
-fn debug_loop(
-    player_query: Query<&Transform, With<PlayerMarker>>,
-    mut npc_query: Query<&mut PositionalTaskQueue>,
+fn follow_main_task_movement_sub_task_planning(
+    mut npc_query: Query<&mut AIMovementTaskQueue>,
+    transform_query: Query<&Transform>,
 ) {
-    let player_transform = player_query.get_single().unwrap();
     for mut task_queue in npc_query.iter_mut() {
-        let task = PositionalTask {
-            target_translation: player_transform.translation,
-        };
-        task_queue.queue.clear();
-        task_queue.queue.push(task);
+        if let Task::Follow(target) = task_queue.main_task {
+            let target_translation = transform_query
+                .get(target)
+                .expect("Cannot find target actor")
+                .translation;
+
+            task_queue.sub_task_queue = vec![SubTask::KeepDistance(target_translation, 2.0)];
+        }
     }
 }
 
-fn init(npc_query: Query<Entity, With<NPCMarker>>, mut commands: Commands) {
-    for entity in npc_query.iter() {
-        commands
-            .entity(entity)
-            .insert(PositionalTaskQueue::default());
+fn work_move_to_sub_task(
+    npc_query: Query<(Entity, &Stats, &AIMovementTaskQueue)>,
+    mut translation_query: Query<&mut Transform>,
+    time: Res<Time>,
+) {
+    for (npc, stats, task_queue) in npc_query.iter() {
+        let first_task = task_queue.sub_task_queue.first();
+        if let Some(SubTask::KeepDistance(target_translation, distance)) = first_task {
+            let mut npc_transform = translation_query.get_mut(npc).expect("Could not find NPC");
+            if target_translation.distance(npc_transform.translation) > *distance {
+                let translation_delta =
+                    npc_transform.forward() * stats.get_movement_velocity() * time.delta_seconds();
+                npc_transform.translation += translation_delta;
+            }
+        }
+    }
+}
+
+fn init(
+    mut commands: Commands,
+    npc_queue: Query<Entity, With<NPCMarker>>,
+    player_queue: Query<Entity, With<PlayerMarker>>,
+) {
+    let player = player_queue.get_single().expect("Could not find Player");
+    for npc in npc_queue.iter() {
+        commands.entity(npc).insert(AIMovementTaskQueue {
+            main_task: Task::Follow(player),
+            ..default()
+        });
     }
 }
