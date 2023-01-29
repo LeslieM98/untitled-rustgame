1c451dc730e6e60e642560aa0a888cb4b33c6daf
Leslie <XXX>
2023-01-29 23:50:38 +0100

    editor disableable

--- src/main.rs
+++ src/main.rs
@@ -15,24 +15,30 @@ use crate::player_ui::PlayerUi;
 use crate::settings::SettingsPlugin;
 use crate::status_event::StatusEventPlugin;
 
+use std::env;
+
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            window: WindowDescriptor {
-                title: "Untiteled Game".to_string(),
-                present_mode: PresentMode::Immediate,
-                ..default()
-            },
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        window: WindowDescriptor {
+            title: "Untiteled Game".to_string(),
+            present_mode: PresentMode::Immediate,
             ..default()
-        }))
-        .add_plugin(PlayerPlugin)
-        .add_plugin(DebugPlugin)
-        .add_plugin(PlayerUi)
-        .add_plugin(EnemyPlugin)
-        .add_plugin(SettingsPlugin)
-        .add_plugin(StatusEventPlugin)
-        .add_startup_system(setup_scene)
-        .run();
+        },
+        ..default()
+    }))
+    .add_plugin(PlayerPlugin)
+    .add_plugin(PlayerUi)
+    .add_plugin(EnemyPlugin)
+    .add_plugin(SettingsPlugin)
+    .add_plugin(StatusEventPlugin)
+    .add_startup_system(setup_scene);
+
+    let args: Vec<String> = env::args().collect();
+    if args.contains(&String::from("debug-editor")) {
+        app.add_plugin(DebugPlugin);
+    }
+    app.run();
 }
 
 fn setup_scene(mut commands: Commands, asset_server: Res<AssetServer>) {
