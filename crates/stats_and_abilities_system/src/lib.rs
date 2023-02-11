dc64483dba14bac4e2cfe02a6c315b75fa9b4131
Leslie <XXX>
2023-02-11 14:51:38 +0100

    fix compile error

--- crates/stats_and_abilities_system/src/lib.rs
+++ crates/stats_and_abilities_system/src/lib.rs
@@ -6,11 +6,6 @@ pub type StatModifierType = f32;
 pub type StatIdentifier = String;
 
 pub mod prelude {
-
-    pub use crate::StatIdentifier;
-    pub use crate::StatModifierType;
-    pub use crate::StatIdentifier;
-
     pub use crate::stats::*;
     pub use crate::health::*;
 }
