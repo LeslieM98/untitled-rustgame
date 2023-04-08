b56d82dcad4542b7b36452b6ddf0a00272910246
Leslie <XXX>
2023-04-08 19:01:43 +0200

    some tests

--- src/network/lobby.rs
+++ src/network/lobby.rs
@@ -2,31 +2,40 @@ use crate::network::server::MAX_CONNECTIONS;
 use bevy::prelude::{Commands, Entity, Resource};
 use bevy::utils::HashMap;
 
+type SpawnFunction = fn(&mut Commands, u64) -> Entity;
+
 #[derive(Resource)]
 pub struct Lobby {
     player_ids: HashMap<u64, Entity>,
-    spawn_player: fn(&mut Commands, u64) -> Entity,
+    spawn_player: SpawnFunction,
 }
 
-pub enum LobbyPacket {
-    SyncConnectedPlayers([Option<u64>; MAX_CONNECTIONS]),
+pub struct SyncConnectedPlayersPackage {
+    ids: [Option<u64>; MAX_CONNECTIONS],
 }
 
 impl Lobby {
-    pub fn new(instantiate_player: fn(&mut Commands, u64) -> Entity) -> Lobby {
+    pub fn new(instantiate_player: SpawnFunction) -> Lobby {
         Lobby {
             spawn_player: instantiate_player,
             player_ids: HashMap::default(),
         }
     }
 
-    pub fn generate_sync_package(&self) -> LobbyPacket {
+    pub fn with_player_ids(player_ids: HashMap<u64, Entity>, spawn_player: SpawnFunction) -> Lobby {
+        Lobby {
+            player_ids,
+            spawn_player,
+        }
+    }
+
+    pub fn generate_sync_package(&self) -> SyncConnectedPlayersPackage {
         let mut data: [Option<u64>; MAX_CONNECTIONS] = [None; MAX_CONNECTIONS];
         for (i, (id, _)) in self.player_ids.iter().enumerate() {
             data[i] = Some(*id);
         }
 
-        LobbyPacket::SyncConnectedPlayers(data)
+        SyncConnectedPlayersPackage { ids: data }
     }
 
     fn handle_disconnected_players(
@@ -74,5 +83,47 @@ impl Lobby {
         mut commands: Commands,
     ) {
         self.handle_disconnected_players(payload, &mut commands);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn dummy_function(_commands: &mut Commands, _player: u64) -> Entity {
+        panic!("This should not ever be called")
+    }
+
+    #[test]
+    fn correct_sync_package() {
+        let mut player_ids: HashMap<u64, Entity> = HashMap::default();
+        player_ids.insert(0, Entity::from_bits(0));
+        player_ids.insert(1, Entity::from_bits(1));
+        player_ids.insert(15, Entity::from_bits(25634));
+
+        let lobby = Lobby::with_player_ids(player_ids.clone(), dummy_function);
+
+        let sync_package = lobby.generate_sync_package();
+
+        assert_eq!(sync_package.ids.len(), MAX_CONNECTIONS);
+
+        let sync_ids: Vec<u64> = sync_package
+            .ids
+            .iter()
+            .filter(|x| x.is_some())
+            .map(|x| x.unwrap())
+            .collect();
+
+        assert_eq!(sync_ids.len(), player_ids.len());
+
+        for sync_id in sync_ids {
+            assert!(player_ids.contains_key(&sync_id));
+        }
+
+        let lobby = Lobby::new(dummy_function);
+        let sync_package = lobby.generate_sync_package();
+        for player in sync_package.ids {
+            assert!(player.is_none())
+        }
     }
 }
