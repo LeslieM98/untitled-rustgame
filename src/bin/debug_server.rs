26156fab9f7488702baab4c417a5ebf17e3c6f1f
Leslie <XXX>
2023-05-04 02:29:24 +0200

    add enemy spawn at server

--- src/bin/debug_server.rs
+++ src/bin/debug_server.rs
@@ -1,6 +1,25 @@
 use bevy::prelude::*;
-use rust_game::gamer_server::GameServer;
+use rust_game::{actor::npc::Enemy, gamer_server::GameServer};
 
 fn main() {
-    App::new().add_plugin(GameServer).run();
+    App::new()
+        .add_plugin(GameServer)
+        .add_startup_system(spawn_enemies)
+        .run();
+}
+
+fn spawn_enemies(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let enemy1 = Enemy::from_pos(
+        Transform::from_xyz(3.0, 1.0, 0.0),
+        &mut meshes,
+        &asset_server,
+        &mut materials,
+    );
+
+    commands.spawn(enemy1);
 }
