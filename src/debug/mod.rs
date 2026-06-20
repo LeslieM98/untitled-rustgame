57bc84e034f2191984a534e6c5d073d6074ce965
Leslie <XXX>
2026-06-21 01:50:13 +0200

    add infinitegrid

--- src/debug/mod.rs
+++ src/debug/mod.rs
@@ -1,6 +1,7 @@
 use bevy::app::{App, Plugin, Startup};
 use bevy::prelude::*;
 use bevy::settings::*;
+use bevy::dev_tools::infinite_grid::{InfiniteGrid, InfiniteGridPlugin};
 
 pub struct DebugPlugin;
 #[derive(Resource, SettingsGroup, Reflect, Default)]
@@ -12,11 +13,14 @@ struct DebugSettings {
 impl Plugin for DebugPlugin {
     fn build(&self, app: &mut App) {
         app
+            .add_plugins(InfiniteGridPlugin)
             .add_systems(Startup, debug_info);
     }
 }
 
-pub fn debug_info() {
+pub fn debug_info(mut commands: Commands) {
+    commands.spawn(InfiniteGrid);
     debug!("Debug Plugin loaded!");
+
 
 }
